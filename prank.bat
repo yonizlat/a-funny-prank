ffa50a3bce16dfe10ac2462719b88f092e03813c
The Starting Guy <yonizlat@gmail.com>
2025-03-21 16:43:33 +0200

    Update prank.bat

--- prank.bat
+++ prank.bat
@@ -6,6 +6,6 @@ powershell -Command "Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name
 
 
 rem you need to change the script below to what you want the bot will say
-echo CreateObject("SAPI.SpVoice").Speak "what sup my nigga, you just got hacked my nigga, dont you worry you is alright, psych my nigga, you is a dumb ass shit, look at your skibidi wallpaper now because my nigger ass changed it nigger, you matherfucker bitch ass nigga, get your ass back to kenya nigger, because you just got hacked , so now suck my dick nigger ,before i kick your ass matherfucker, suck my nigger dick now" > speak.vbs
+echo CreateObject("SAPI.SpVoice").Speak "TYPE HERE WHATEVER YOU WANT THE BOT TO SAY" > speak.vbs
 cscript //nologo speak.vbs
 del speak.vbs
